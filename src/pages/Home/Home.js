191ea66ae3892ae1fbcb88d3e667cf3621cb8dc3
Vinay846 <vinayvin84629@gmail.com>
2021-08-04 03:50:14 +0530

    Fix home Page Styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import { UserCheck, Trash2, Gift } from 'react-feather';
 import {TopSection, BottomSection} from '../../components/Styles/styles';
+import styled from 'styled-components';
+import AuthContext from '../../context/auth-context';
+import { useHistory } from 'react-router-dom';
+
+const StyledDiv = styled.div`
+    cursor: pointer;
+    &:hover {
+        color: green;
+    }
+
+    @media (max-width: 768px) {
+        span {
+            margin-left: 3.4rem;
+        }
+    }
+`;
+
 
 function Home() {
+    const context = useContext(AuthContext);
+    const history = useHistory();
+
+    const handleClickAccount=()=>{
+        if(context.token !== null){
+            history.push('/profile');
+        }else{
+            history.push('/login');
+        }
+    }
+    
+    const handleClickPoints=()=>{
+        if(context.token !== null){
+            history.push('/points');
+        }else{
+            history.push('/login');
+        }
+    }
+
     return (
         <>
         <TopSection>
@@ -10,18 +46,18 @@ function Home() {
             <h2>By Earning Rewards</h2>
         </TopSection>
         <BottomSection>
-            <div>
-                <UserCheck size={200} strokeWidth={0.2}/>
+            <StyledDiv  onClick={handleClickAccount}>
+                <span><UserCheck size={200} strokeWidth={0.2}/></span>
                 <p>MAKE ACCOUNTS WITH US</p>
-            </div>
-            <div>
+            </StyledDiv>
+            <StyledDiv onClick={() => history.push('/dustbins')}>
                 <Trash2 size={200} strokeWidth={0.2} fillOpacity="10%"/>
                 <p>USE OUR SMARTDUSTBIN</p>
-            </div>
-            <div>
+            </StyledDiv>
+            <StyledDiv onClick={handleClickPoints}>
                 <Gift size={200} strokeWidth={0.2}/>
                 <p>EARN POINTS</p>
-            </div>
+            </StyledDiv>
         </BottomSection>
         </>
     )
